4ceae7b575c517b7fc2f5615156bbc2cd9261f00
Hitesh Kumar Mandal <55690134+HiteshBucky@users.noreply.github.com>
2020-11-03 00:04:36 +0530

    Added Flatten doubly linked list

---
+++ DS_Algo/Week_2/Serching/Flatten_Multilevel_Doubly_Linked_List.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+	public Node flatten(Node head) {
+		if (head == null) return head;
+
+		Stack<Node> stack = new Stack<>();
+		stack.push(head);
+		Node prev = null;
+
+		while (!stack.empty()) {
+			Node node = stack.pop();
+			if (node.next != null) stack.push(node.next);
+
+			if (node.child != null) {
+				stack.push(node.child);
+				node.child = null;
+			}
+			node.next = stack.empty() ? null : stack.peek();
+			node.prev = prev;
+			prev = node;
+		}
+
+		return head;
+	}
+}
